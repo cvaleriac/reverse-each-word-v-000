720b504a71cbe3d708c0ca0a26f60b0945087f52
valeria campilongo <cvaleriac@hotmail.it>
2018-04-15 20:59:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
-  reversed_sentence = (sentence.split(' ').each do |word|
+  reversed_sentence = sentence.split(' ').collect do |word|
     word.reverse
-  end).join(" ")
+  end
+  reversed_sentence.join(" ")
   end
